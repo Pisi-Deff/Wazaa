bd23919802a37e8f8f998ddbf88a3cf53266b6eb
Eerik <eerik46@gmail.com>
2013-09-13 01:19:33 +0300

    Add stub for HTTPClient

---
+++ src/wazaa/http/HTTPClient.java
@@ -0,0 +1,17 @@
+package wazaa.http;
+
+/**
+ * Class for sending HTTP requests to other Wazaas.
+ * Propagates searchfile requests, sends foundfiles, gets files.
+ * @author Eerik
+ */
+public class HTTPClient extends Thread {
+	public HTTPClient() {
+		
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+}
