57126075f99cdf78be6d4d5a8a813bb09a1cf480
Eerik <eerik46@gmail.com>
2013-10-09 19:51:48 +0300

    Store controllers for all scenes and give access to them.

--- src/wazaa/ui/ScenesController.java
+++ src/wazaa/ui/ScenesController.java
@@ -5,23 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javafx.fxml.FXMLLoader;
-//import javafx.fxml.Initializable;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ScenesController extends StackPane {
 	private Map<String, Parent> scenes = new HashMap<String, Parent>();
-//	private Map<String, Initializable> controllers = 
-//			new HashMap<String, Initializable>();
+	private Map<String, Initializable> controllers = 
+			new HashMap<String, Initializable>();
 	
 	public void addSceneFromFXML(String sceneName, String fxmlFilename) 
 			throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFilename));
 		Parent scene = (Parent) loader.load();
 		scenes.put(sceneName, scene);
-//		controllers.put("sceneName",
-//				(Initializable) loader.getController());
+		controllers.put(sceneName,
+				(Initializable) loader.getController());
 	}
 	
 	public void switchToScene(String sceneName) {
@@ -32,5 +32,9 @@ public class ScenesController extends StackPane {
 		getChildren().add(scenes.get(sceneName));
 		getScene().getWindow().sizeToScene();
 		((Stage) getScene().getWindow()).show();
+	}
+	
+	public Initializable getController(String sceneName) {
+		return controllers.get(sceneName);
 	}
 }
