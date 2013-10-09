3f79c08dcda5b14b91561dd5479850aadb2472a1
Eerik <eerik46@gmail.com>
2013-10-09 22:17:00 +0300

    Show machines list on MainScreen.

--- src/wazaa/ui/MainScreen.java
+++ src/wazaa/ui/MainScreen.java
@@ -1,9 +1,15 @@
 package wazaa.ui;
 
 import java.net.URL;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import wazaa.Machine;
+import wazaa.Wazaa;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,19 +19,19 @@ import javafx.scene.control.TableView;
 
 public class MainScreen implements Initializable {
     @FXML
-    private Button addMachineButton;
-
-    @FXML
     private TableView<?> fileSearchTable;
 
     @FXML
     private TableView<?> sharedFilesTable;
 
     @FXML
-    private ListView<Machine> MachinesList;
+    private ListView<Machine> machinesList;
     
     @FXML
     private Label listeningStatusLabel;
+    
+    @FXML
+    private Button addMachineButton;
     
     @FXML
     private Button addMachinesFromFileButton;
@@ -38,7 +44,14 @@ public class MainScreen implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		machinesList.setItems(
+						FXCollections.observableList(Wazaa.getMachines()));
+		addMachineButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				
+			}
+		});
 	}
 
 	public void setListeningStatusLabelText(String text) {
