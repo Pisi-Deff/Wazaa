d3f7a53abc69a28d857c6d317325b9e6cb9424d6
Eerik <eerik46@gmail.com>
2013-10-09 15:43:27 +0300

    Clear GUI class of old imports.

--- src/wazaa/ui/WazaaJFXGUI.java
+++ src/wazaa/ui/WazaaJFXGUI.java
@@ -1,23 +1,10 @@
 package wazaa.ui;
 
 import wazaa.Wazaa;
-import wazaa.http.HTTPServer;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class WazaaJFXGUI extends Application {
