9643ebf37d8fce3c25c50ba97eda6c9decbe4dd6
Eerik <Eerik@Eerik-LAPPY>
2013-10-10 10:05:20 +0300

    Make machinesList refresh properly on use of remove button.

--- src/wazaa/ui/MainScreen.java
+++ src/wazaa/ui/MainScreen.java
@@ -5,14 +5,17 @@ import java.util.ResourceBundle;
 
 import wazaa.Machine;
 import wazaa.Wazaa;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 
 public class MainScreen implements Initializable {
@@ -42,15 +45,48 @@ public class MainScreen implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		machinesList.getSelectionModel().
+			setSelectionMode(SelectionMode.MULTIPLE);
 		machinesList.setItems(
 						FXCollections.observableList(Wazaa.getMachines()));
-		addMachineButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				
-			}
-		});
+		machinesList.getSelectionModel().selectedItemProperty().addListener(
+				new ChangeListener<Machine>() {
+					@Override
+					public void changed(
+							ObservableValue<? extends Machine> observable,
+							Machine oldValue, Machine newValue) {
+						if (newValue == null) {
+							removeSelectedMachinesButton.setDisable(true);
+						} else {
+							removeSelectedMachinesButton.setDisable(false);
+						}
+					}
+				});
 	}
+	
+    @FXML
+    private void addMachineButtonAction(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    private void addMachinesFromFileButtonAction(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    private void addMachinesFromURLButtonAction(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    private void removeSelectedMachinesButtonAction(ActionEvent event) {
+		Wazaa.getMachines()
+			.removeAll(machinesList.getSelectionModel().getSelectedItems());
+		ObservableList<Machine> list = machinesList.getItems();
+		machinesList.setItems(null);
+		machinesList.setItems(list);
+    }
 
 	public void setListeningStatusLabelText(String text) {
 		listeningStatusLabel.setText(text);
